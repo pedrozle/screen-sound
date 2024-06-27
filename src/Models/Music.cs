6e1ff2a21c11c1c7b86331e4b76698902a559f56
Pedro <jpedro@tjro.jus.br>
2024-06-27 10:26:29 -0400

    refatorado pastas

---
+++ src/Models/Music.cs
@@ -0,0 +1,16 @@
+namespace ScreenSound;
+
+class Music(string name, string artist, string album, int duration, bool available)
+{
+    public string Name { get; } = name;
+    public string Artist { get; } = artist;
+    public string Album { get; } = album;
+    public int Duration { get; } = duration;
+    public bool Available { get; } = available;
+
+    override public string ToString()
+    {
+        return $"Título: {Name}, Artista: {Artist}, Album: {Album} Duração: {Duration}, Disponível:  {(Available ? "Disponível na assinatura" : "Assine para ter acesso")}";
+    }
+
+}
